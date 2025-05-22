6f4c77400fa7e49d974b4330588d02169036f228
Jatin5760 <142175713+Jatin5760@users.noreply.github.com>
2025-05-22 17:04:58 +0530

    Commit

---
+++ 09_CRUD/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+// C - Create => POST(method)
+// R - Read => GET(method)
+// U - Update => PUT(method)
+// D - Delete => DELETE(method)
+
+const app = express();
+
+
+app.get('/', (req, res) =>{
+    app.send('This is response');
+})
+
+app.post('/create', (req, res) => {
+    app.post('This is a POST request');
+});
+
+app.put('/update', (req, res) => {
+    app.put('This is a PUT request');
+});
+
+app.delete('/delete', (req, res) => {
+    app.delete('This is a DELETE request');
+});
+
+
+const port = 3000;  
+app.listen(port, () => { 
+    console.log(`Server is running on http://localhost:${port}`);
+});

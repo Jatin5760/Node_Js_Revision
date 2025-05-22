0a10e0d68eb130d5a0892e40d6dee21b9a07a648
Jatin5760 <142175713+Jatin5760@users.noreply.github.com>
2025-05-22 21:46:32 +0530

    Commit

--- 13_Form_Handling/server.js
+++ 13_Form_Handling/server.js
@@ -2,34 +2,28 @@ import express from "express";
 import path from 'path';
 const app = express();
 
-
 app.use(express.static(path.join(path.resolve(), 'public')));
-
-
-// This is route to render the index.ejs file
-app.get('/', (req, res)=>{
-    res.render('index.ejs');
-})
-
-
-
-// This is a route to handle form submission
-app.post('/form-submit', (req, res) => {
-    res.json({
-        message: "Form submitted successfully",
-        success: true,
-    })
-});
-
-
 
 
 // How to hide url during form submission?
 // We can do this by using express.urlencoded() middleware
 // This middleware is used to parse the urlencoded data
+app.use(express.urlencoded({extended: true}));
 
+// This is route to render the index.ejs file
+app.get('/', (req, res)=>{
+    res.render('index.ejs');
 
-app.use(express.urlencoded({ extended: true }));
+})
+
+// This is a route to handle form submission
+app.post('/form-submit', (req, res) => {
+    console.log(req.body);
+    res.json({
+        message: "Form submitted successfully",
+        success: true,
+    })
+});
 
 
 

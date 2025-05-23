4ac8c8e7d556d22052d57d5b47f8ae345be4e208
Jatin5760 <142175713+Jatin5760@users.noreply.github.com>
2025-05-23 21:27:28 +0530

    Commit

--- 16_URL_shortner/Controllers/url.js
+++ 16_URL_shortner/Controllers/url.js
@@ -1,7 +1,6 @@
 import { Url } from "../Models/Url.js";
 import shortid from "shortid";
 
-
 export const shortUrl = async (req, res) => {
   const longUrl = req.body.longUrl;
   const shortCode = shortid.generate();
@@ -15,14 +14,22 @@ export const shortUrl = async (req, res) => {
   });
   await newUrl.save();
 
-
   console.log("Shortened URL:", newUrl);
 
   res.render("index.ejs", { shortUrl: shortUrl });
-  
 };
 
-
 export const getOriginalUrl = async (req, res) => {
-    
-}
+  const shortCode = req.params.shortCode;
+
+  // find on Database
+  const originalUrl = await Url.findOne({ shortCode });
+
+  if (originalUrl) {
+    res.redirect(originalUrl.longUrl);
+  } else {
+    res.json({
+      message: "Invalid Short Code",
+    });
+  }
+};

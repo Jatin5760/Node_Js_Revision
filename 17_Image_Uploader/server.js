88a92e6940feb5ffa4dcbf0a36387187d29b8e59
Jatin5760 <142175713+Jatin5760@users.noreply.github.com>
2025-05-23 22:45:14 +0530

    Commit

--- 17_Image_Uploader/server.js
+++ 17_Image_Uploader/server.js
@@ -1,7 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import path from "path";
 
 const app = express();
+
+cloudinary.config({
+  cloud_name: "dywpgraas",
+  api_key: "285195566768468",
+  api_secret: "Rck844OYA01cOqo9aMYmkRWhRhA",
+});
+
 app.use(express.urlencoded({ extended: true }));
 
 mongoose
@@ -14,15 +24,32 @@ mongoose
   .then(() => console.log("MongoDb Connected"))
   .catch((err) => console.log(err));
 
-
-
 // Render ejs file
 app.get("/", (req, res) => {
-  res.render("index.ejs",{Url:null});
+  res.render("index.ejs", { Url: null });
 });
 
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+  destination: "./public/uploads",
 
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + path.extname(file.originalname);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
 
+const upload = multer({ storage: storage });
+
+// Upload file to cloudinary
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const file = req.file.path;
+  res.json({
+    message: "File uploaded successfully",
+  });
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+});
 
 const port = 3000;
 app.listen(port, () => {

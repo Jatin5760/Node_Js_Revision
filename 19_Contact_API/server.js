83acf55ab2a5c3b37309bf3f1cbef2af92cebcd0
Jatin5760 <142175713+Jatin5760@users.noreply.github.com>
2025-05-24 19:22:55 +0530

    Commit

--- 19_Contact_API/server.js
+++ 19_Contact_API/server.js
@@ -1,23 +1,41 @@
-import express from 'express';
-import mongoose from 'mongoose';
-
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import { User } from "./Models/User.js";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Home Route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Contact API - Working fine!" });
+});
 
 // User Routes - start with /api its a good practice
 // @api name : user register
 // @api method : POST
 // @api endpoint : /api/register
 
-app.post("/api/user/register", (req,res) =>{
-    const {name, email, password} = req.body;
-    
-})
+app.post("/api/user/register", async (req, res) => {
+  const { name, email, password } = req.body;
 
+  if (name == "" || email == "" || password == "") 
+    return res.json({ message: "Please fill all the fields!" });
+  
 
+  let user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  res.json({
+    message: "User Created Successfully",
+    user,
+    success: true,
+  });
+});
 
 // Connecting to MongoDB
 mongoose
@@ -30,12 +48,7 @@ mongoose
   .then(() => console.log("MongoDb Connected"))
   .catch((err) => console.log(err));
 
-
-
-
-
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
-    

2a3826b1d4e8e0b31caacc7d7c05daefc9c84253
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-26 01:19:43 +0530

    Commit

--- 00_Javascript_Mastery/02_Variables/variable.js
+++ 00_Javascript_Mastery/02_Variables/variable.js
@@ -1,4 +1,22 @@
 // Variables are just like containers to stroe the data
 
+// Let, const and var are used to declare variables in JavaScript.
+// Let - Can be re-declared and updated. A block scope variable.
+// Const - Cannot be re-declared or updated. A block scope variable.
+// Var - Can be re-declared and updated. A function scope variable.
+
+
+
+// Datatypes in JavaScript can be categorized into two main types:
 // 1) Number [{-infinity, +infinity}]
 // 2) Strings ['a', '1', '@', 'hello']
+// 3) Boolean [true, false]
+// 4) Undefined [undefined]
+// 5) Null [null]
+// 6) BigInt [1234567890123456789012345678901234567890n]
+// 7) Symbol [Symbol('description')]
+// 8) Object [{key: value}]
+// 9) Array [1, 2, 3, 'a', 'b', 'c']
+// 10) Function [function() { return 'Hello'; }]
+
+// Javascript types are dynamic, meaning you can change the type of a variable at any time.

4937bde1f43abe0fa44f5fad3e8d648f40fd5655
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-27 18:13:59 +0530

    Commit

--- 00_Javascript_Mastery/04_Conditionals/index.js
+++ 00_Javascript_Mastery/04_Conditionals/index.js
@@ -42,9 +42,17 @@ switch (day) {
 
 
 // While Loop
-let i = 0;
-while(i < 5){
-    console.log(i);
-    i ++
-}
+// let i = 0;
+// while(i < 5){
+//     console.log(i);
+//     i ++
+// }
 
+
+// Do While Loop
+let j = 0;
+
+do{
+    console.log(j);
+    j++;
+}while(j < 5);

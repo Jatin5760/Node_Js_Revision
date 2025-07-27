d11728057e9558be3dea6eb76edd0b863baa8925
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-27 12:31:36 +0530

    Commit

--- 00_Javascript_Mastery/03_Operators/index.js
+++ 00_Javascript_Mastery/03_Operators/index.js
@@ -1,13 +1,14 @@
 // Operators
 
 // Arithmatic
+
 let a = 10;
 let b = 20;
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
 
 
 // Assignment Operator

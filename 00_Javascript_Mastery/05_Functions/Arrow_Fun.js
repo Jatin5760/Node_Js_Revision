7743af184f24bc57af9b57ca86143b0fcb4c7d0c
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-27 23:11:11 +0530

    Commit

--- 00_Javascript_Mastery/05_Functions/Arrow_Fun.js
+++ 00_Javascript_Mastery/05_Functions/Arrow_Fun.js
@@ -1,2 +1,16 @@
 // Arrow function 
 console.log("Learning Arrow Functions");
+
+const print = () => console.log("Jatin");
+print();
+
+
+// let sum = (a,b) => console.log(a + b);
+// sum(5,15);
+
+const sum = (a,b) => {
+    return a + b;
+}
+let ans = sum(10,30)
+console.log(ans);
+

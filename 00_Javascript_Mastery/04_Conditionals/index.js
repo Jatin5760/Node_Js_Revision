2f1666d4ff056002a6ab1be46cb09467cd6c3905
Jatin5760 <jatinsharma14202003@gmail.com>
2025-07-27 13:06:50 +0530

    Commit

--- 00_Javascript_Mastery/04_Conditionals/index.js
+++ 00_Javascript_Mastery/04_Conditionals/index.js
@@ -8,3 +8,33 @@ if (age < 18){
 else{
     console.log("You are adult");
 }
+
+
+// Swtich cases
+let day = 7;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        break;
+}

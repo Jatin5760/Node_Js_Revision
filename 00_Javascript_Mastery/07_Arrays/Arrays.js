da946bc18888deff19642e590bdccdb3def1ab13
Jatin5760 <jatinsharma14202003@gmail.com>
2025-08-03 18:51:16 +0530

    Commit

--- 00_Javascript_Mastery/07_Arrays/Arrays.js
+++ 00_Javascript_Mastery/07_Arrays/Arrays.js
@@ -1,19 +1,41 @@
 let arr = [10, 20, 30, 40]
-console.log(arr);
+// console.log(arr);
 
+// Push : Add elements at the end of array
 arr.push(50)
-console.log(arr);
+// console.log(arr);
 
-
+// Remove element from the end of array
 arr.pop()
-console.log(arr);
+// console.log(arr);
 
-
+// Remove element from the starting
 arr.shift()
-console.log(arr);
+// console.log(arr);
 
+// Add elements in the starting of array
 arr.unshift(69)
-console.log(arr);
+// console.log(arr);
+
+// Find : Return the first element that satisfies the provided testing function
+let array = [1,2,3,4]
+let found = array.find((element) => element > 2)
+console.log(found);
+
+// Includes : Return true or false
+console.log(arr.includes(30)); // True
+console.log(arr.includes(50)); // False
+
+// Concat
+
+let arr1 = [10, 20]
+let arr2 = [30, 40]
+
+let merged = arr1.concat(arr2)
+console.log(merged);
+
+
+
 
 
 

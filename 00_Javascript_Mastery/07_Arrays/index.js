40b8ed1109be056b6dcb2b7e921cdf3a303c8544
Jatin5760 <jatinsharma14202003@gmail.com>
2025-08-03 11:12:20 +0530

    Commit

---
+++ 00_Javascript_Mastery/07_Arrays/index.js
@@ -0,0 +1,11 @@
+// console.log("Learning Arrays");
+
+let arr = ["Jatin", 23, 30.5, 'A']
+let number = ['ram', 10, 20 ,30 ,40]
+// console.log(number[4]);
+
+
+for(let i = 0; i < number.length; i++){
+    console.log(number[i]);
+    
+}

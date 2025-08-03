2d8ba94d35f3cf7c6acf1d1341682d3a99a1f0fd
Jatin5760 <jatinsharma14202003@gmail.com>
2025-08-03 19:57:49 +0530

    Commit

--- 00_Javascript_Mastery/07_Arrays/High_Order_Methods.js
+++ 00_Javascript_Mastery/07_Arrays/High_Order_Methods.js
@@ -1,4 +1,4 @@
-// Map
+// Map : Map also returns an array
 let arr = [1,2,3,4]
 let result = arr.map((e)=>e*2)
 console.log(result);
@@ -6,3 +6,9 @@ console.log(result);
 // Filter : Return a new array that fulfill the following filter condition
 let result1 = arr.filter((el)=>el>=2)
 console.log(result1);
+
+// Reduce : Reduces an array to a single value by applying function to each element
+let arr2 = [10,20,30,40]
+let sum = arr2.reduce((prev,el)=>prev+el, 0)
+console.log(sum);
+

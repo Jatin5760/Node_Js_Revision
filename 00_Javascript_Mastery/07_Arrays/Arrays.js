45625b6d59690681d156be532fd8829b5c4ea431
Jatin5760 <jatinsharma14202003@gmail.com>
2025-08-03 19:17:36 +0530

    Commit

--- 00_Javascript_Mastery/07_Arrays/Arrays.js
+++ 00_Javascript_Mastery/07_Arrays/Arrays.js
@@ -20,19 +20,28 @@ arr.unshift(69)
 // Find : Return the first element that satisfies the provided testing function
 let array = [1,2,3,4]
 let found = array.find((element) => element > 2)
-console.log(found);
+// console.log(found);
 
 // Includes : Return true or false
-console.log(arr.includes(30)); // True
-console.log(arr.includes(50)); // False
+// console.log(arr.includes(30)); // True
+// console.log(arr.includes(50)); // False
 
 // Concat
-
 let arr1 = [10, 20]
 let arr2 = [30, 40]
-
 let merged = arr1.concat(arr2)
-console.log(merged);
+// console.log(merged);
+
+// Join
+let arr3 = ["Jatin", "SDE", "Amazon", "Microsoft"]
+let result = arr3.join(" : ")
+// console.log(result);
+
+// Splice
+let arr4 = [1,2,3,4]
+let element = arr.splice(2,1,'a')
+console.log(arr4);
+
 
 
 

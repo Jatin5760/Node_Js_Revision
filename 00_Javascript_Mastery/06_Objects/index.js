8d19e02dfac79247f746c6000a02d9253e17dd4d
Jatin5760 <jatinsharma14202003@gmail.com>
2025-08-03 10:59:52 +0530

    Commit

--- 00_Javascript_Mastery/06_Objects/index.js
+++ 00_Javascript_Mastery/06_Objects/index.js
@@ -1,1 +1,24 @@
-console.log("Learning Objects");
+// console.log("Learning Objects");
+
+const person = {
+    name: 'Jatin',
+    age: 501,
+    phone: 'Samsumg Galaxy s23',
+    income: "$230 B",
+};
+
+const car1 = {
+    name: "BMW",
+    color: "Blue",
+    price: "80L",
+};
+
+
+// Spread Operator : Ye jab use hota hai jab hame ek object se ya ek array se object copy karna ho dusre object mai.
+const car2 = {...car1, name: "Fortuner"};
+
+console.log(person.name);
+console.log(car2);
+
+
+

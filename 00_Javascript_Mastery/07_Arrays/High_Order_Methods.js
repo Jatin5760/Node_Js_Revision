ff5aa7d46e316deb5e32ef3e4eb8a26b8ab965c0
Jatin5760 <jatinsharma14202003@gmail.com>
2025-08-03 19:54:20 +0530

    Commit

---
+++ 00_Javascript_Mastery/07_Arrays/High_Order_Methods.js
@@ -0,0 +1,8 @@
+// Map
+let arr = [1,2,3,4]
+let result = arr.map((e)=>e*2)
+console.log(result);
+
+// Filter : Return a new array that fulfill the following filter condition
+let result1 = arr.filter((el)=>el>=2)
+console.log(result1);

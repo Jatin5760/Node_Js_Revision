334f3ef5b0b0d3140e708f35f032cab4717e1095
Jatin5760 <jatinsharma14202003@gmail.com>
2025-08-03 23:54:45 +0530

    Commit

--- 00_Javascript_Mastery/07_Arrays/Advanced_Loops.js
+++ 00_Javascript_Mastery/07_Arrays/Advanced_Loops.js
@@ -1,1 +1,14 @@
-console.log("I'm Learning advance Loops");
+// console.log("I'm Learning advance Loops");
+
+const phone = {
+    brand:'iphone',
+    model:16,
+    price: 75000,
+    ram: "8gb",
+    rom: "128gb",
+};
+
+// For in loop : Generally print the key value of the loop
+for(let key in phone){
+    console.log(key);
+}
